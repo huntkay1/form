3a179ab856f2697482ee1df3014b6e9e12d2840c
Kayla Hunt <huntkay1@msu.edu>
2024-02-09 18:10:54 -0500

    Fix confirm password focus color

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ const passwordConf = document.querySelector("#confirm-pass");
 const errorMessage = document.querySelector(".error-message");
 
 function validatePassword() {
-    if (passwordConf.value === "" || passwordInput.value === passwordConf.value) {
+    if (passwordConf.value === "" || passwordInput.value === passwordConf.value) {   
         errorMessage.innerHTML = "";
-        passwordConf.style.border = "1px solid #E5E7EB";
+        passwordConf.style.borderColor = "";
+        
     } else {
         errorMessage.innerHTML = "*Passwords do not match";
-        passwordConf.style.border = "1px solid red";
+        passwordConf.style.borderColor = "red";
     };
+
 }
+
 
 passwordInput.onkeyup = validatePassword;
 passwordConf.onkeyup = validatePassword;
